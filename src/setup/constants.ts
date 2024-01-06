13d8563a718629d922ebe280e57e68d5e6aba1a5
lau1944 <a954723219@gmail.com>
2024-01-06 13:36:49 +0800

    feat: add donation

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "";
 export const GITHUB_LINK = "";
-export const DONATION_LINK = "patreon.com/ruidot";
+export const DONATION_LINK = "https://patreon.com/ruidot";
 export const GA_ID = "G-44YVXRL61C";
 export const BACKEND_URL = "https://backend.movie-web.app";
